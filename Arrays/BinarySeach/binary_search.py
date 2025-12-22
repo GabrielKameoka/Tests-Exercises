d9b03ae8791ed50c68b56a4a73b25eb35c22e10a
GabrielKameoka <gabrielkameoka@gmail.com>
2025-12-22 09:56:14 -0300

    Binary Search img

--- Arrays/BinarySeach/binary_search.py
+++ Arrays/BinarySeach/binary_search.py
@@ -4,7 +4,7 @@ def binary_search(nums, n):
     steps = 0
 
     while low < high:
-        steps += 1
+        steps += 1  
         mid = int((low + high)/2)
 
         if nums[mid] == n:
@@ -17,9 +17,9 @@ def binary_search(nums, n):
 
     return - 1
 
-a = [1, 2, 3, 4, 5]
+a = [1, 2, 3, 4, 5, 6]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-binary_seach(c, 3)
+binary_search(c, 3)
